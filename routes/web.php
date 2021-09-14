b5a07ddccd90a82fa0fac4f17bb89cf0227bb99d
Davut <dsergeeb@smartdatatech.io>
2021-09-14 21:36:29 +0300

    Added base page

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+$commonNamespace = '\App\Http\Controllers';
+
+Route::namespace($commonNamespace)->group(function() {
+    Route::get('/', 'HomeController@index')->name('home');
 });
