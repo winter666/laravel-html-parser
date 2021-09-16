2d398fb7f299dc4e6a8b38a96e7f7610e8183d9f
Davut <dsergeeb@smartdatatech.io>
2021-09-16 21:42:07 +0300

    Fixed Loader and Start Loader on Scheduler every hour

--- app/Console/Commands/Loader.php
+++ app/Console/Commands/Loader.php
@@ -5,11 +5,12 @@ namespace App\Console\Commands;
 use App\Interfaces\PageParser;
 use App\Services\LoadService;
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Log;
 
 class Loader extends Command
 {
 
-    protected $signature = 'load:news {--domain=}';
+    protected $signature = 'load:news {--service=}';
 
     protected $description = 'Load Contents';
 
@@ -21,36 +22,50 @@ class Loader extends Command
      */
     public function handle()
     {
-        $domain = $this->option('domain');
-        if ($domain) {
-            // Если домен указан то подгружаем по определенному сервису
-            $loadService = LoadService::getServiceByKey($domain);
+        $service = $this->option('service');
+        if ($service) {
+            // Если сервис указан то подгружаем по определенному сервису
+            $loadService = LoadService::getServiceByKey($service);
             if ($loadService) {
                 // Если этот сервис найден
-                $this->printStart($domain);
-                $this->runOne(new $loadService());
+                $this->printStart($service);
+                $this->runOne(new $loadService(), $service);
             } else {
-                $this->error('Undefined service ' . $domain);
+                $this->error('Undefined service ' . $service);
                 return 1;
             }
         } else {
             // Если домен не указан - то подгружаем по всему списку
             $loadServices = LoadService::ALLOW_LOAD_SERVICES;
 
-            foreach ($loadServices as $domain => $service) {
-                $this->printStart($domain);
-                $this->runOne(new $service());
+            foreach ($loadServices as $serviceKey => $serviceClass) {
+                $this->runOne(new $serviceClass(), $serviceKey);
             }
         }
         return 0;
     }
 
-    private function runOne(PageParser $pageParser) {
-        $pageParser->run();
-        $pageParser->saveData();
+    private function runOne(PageParser $pageParser, $serviceKey) {
+        $this->printStart($serviceKey);
+        try {
+            $pageParser->run();
+            $pageParser->saveData();
+            $this->printEnd($serviceKey);
+        } catch (\Exception $e) {
+            $this->printFailed($serviceKey);
+            Log::error('On sync ' . $serviceKey . " " . $e->getMessage());
+        }
     }
 
-    private function printStart($domain) {
-        $this->alert('Run for '.$domain.' service');
+    private function printStart($service) {
+        $this->info('Run for '.$service.' service');
+    }
+
+    private function printEnd($service) {
+        $this->info('End for '.$service.' service');
+    }
+
+    private function printFailed($service) {
+        $this->error('Failed for '.$service.' service');
     }
 }
