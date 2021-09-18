5ba679fba817e776588068a569335cae7dd4970c
Davut <dsergeeb@smartdatatech.io>
2021-09-18 03:38:28 +0300

    Relationship

--- app/Models/Parser/News.php
+++ app/Models/Parser/News.php
@@ -2,19 +2,30 @@
 
 namespace App\Models\Parser;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @property $id
+ * @property $source_link
+ * @property $content
+ * @property $topic
+ * @property $external_datetime
+ * @property $load_service
+ * @property $news_detail_id
+ **/
 class News extends Model
 {
-    use HasFactory;
-
     protected $table = 'news';
     protected $fillable = [
         'source_link',
         'content',
         'topic',
         'external_datetime',
-        'load_service'
+        'load_service',
+        'news_detail_id'
     ];
+
+    public function detail() {
+        return $this->hasOne(NewsDetail::class, 'news_id', 'id');
+    }
 }
